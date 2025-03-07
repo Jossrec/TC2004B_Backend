1d78f68430209d5fbfcd1c8631d0c0931dad051e
Jossrec <a00837812@tec.mx>
2025-03-06 22:23:58 -0600

    login details

--- controllers/login.controllers.js
+++ controllers/login.controllers.js
@@ -4,6 +4,13 @@ export const login = async (req, res) => {
     const pool = await sqlConnect();
     const data = await pool.request().input("username", sql.VarChar, req.body.username).query('SELECT * FROM users WHERE username = @username');
     console.log(data.recordset);
+    console.log(data)
+    console.log(req.body.password.length);
+    console.log(data.recordset[0].password.length)
     let isLogin = data.recordset[0].password === req.body.password;
-    res.status(200).json({ isLogin: isLogin });
+    if (isLogin) {
+        res.status(200).json({ isLogin: isLogin, user: data.recordset[0] });
+    } else {
+        res.status(400).json({ isLogin: isLogin, user: {} });
+    }
 };
