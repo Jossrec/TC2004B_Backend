89fc74b1f0a888aeb9371c40c1c4e70623279234
Jossrec <a00837812@tec.mx>
2025-05-20 19:44:00 -0600

    login put

--- routes/login.routes.js
+++ routes/login.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { login, register, updatePassword } from "../controllers/login.controllers.js";
+import { login, register, putlogin} from "../controllers/login.controllers.js";
 
 const router = Router();
 
 router.post("/login/", login);
 router.post("/register/", register);
-router.put("/updatePassword/", updatePassword);
+router.put("/login/:password", putlogin);
 
 export default router;
