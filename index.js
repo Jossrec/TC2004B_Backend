df0adbcfa376e7121a8b4c6582d029f7aecd4943
Jossrec <a00837812@tec.mx>
2025-05-20 19:30:16 -0600

    index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import "dotenv/config"
 import express from "express";
-import indexRouter from "./routes/index.router.js"
-import itemsRouter from "./routes/items.router.js"
-import loginRouter from "./routes/login.router.js"
+import indexRouter from "./routes/index.routes.js"
+import itemsRouter from "./routes/items.routes.js"
+import loginRouter from "./routes/login.routes.js"
 import cors from "cors";
 import morgan from "morgan";
 
